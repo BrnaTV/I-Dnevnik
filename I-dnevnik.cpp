e21cfd38d212e33b4b400cf63f7542d5304a76c2
bundevaf <bundevaf@gmail.com>
2023-06-11 18:21:19 +0200

    popravak

--- I-dnevnik.cpp
+++ I-dnevnik.cpp
@@ -6,148 +6,207 @@
 #include <string>
 #include <cstdlib>
 using namespace std;
+
 void pause()
 {
-    cout << endl << "Pritisnite enter za nastavak!";
-    string dummy;
-    cin.ignore();
-    getline(cin, dummy);
+	cout << endl << "Pritisnite enter za nastavak!";
+	string dummy;
+	cin.ignore();
+	getline(cin, dummy);
 }
+
 int main()
 {
-    unsigned long long int* redni = new unsigned long long int[1000];
-    string* prezimeIme = new string[1000];
-    string* rubrika = new string[1000];
-    string* predmet = new string[1000];
-    int izbor;
-    int br = 0;
-    srand(time(NULL));
-    while (1)
-    {
-        system("cls");
-        cout << "Dobrodosli u I-dnevnik\n";
-        cout << "1. Unos ucenika " << endl;
-        cout << "2. Ispis svih ucenika " << endl;
-        cout << "3. Unos predmeta " << endl;
-        cout << "4. Unos novih ocjena " << endl;
-        cout << "5. Slucajni odabir " << endl;
-        cout << "6. Brisanje ucenika " << endl;
-        cout << "7. Izlaz iz programa " << endl;
-        cout << "Vas odabir je: " ;
-        cin >> izbor;
-        if (izbor == 1)
-        {
-            cout << "Unesite redni broj ucenika: ";
-            cin >> redni[br];
-            cin.ignore();
-            cout << "Unesite ime i prezime ucenika: ";
-            getline(cin, prezimeIme[br]);
-            br++;
-        }
-        else if (izbor == 2)
-        {
-			if (br > 0)
-			{
-				for (int i = 0; i < br; i++)
-				{
-					cout << "Redni broj ucenika: " << redni[i] << endl;
-					cout << "Ime i Prezime ucenika: " << prezimeIme[i] << endl;
+	string predmeti[] = { "AiP", "Hrvatski", "Matematika", "Fizika", "Povijest" };
+	string* ucenici = new string[50];
+	int* dnevnik = new int[1000];
+	int izbor;
+	int brojUcenika = 0;
+	int brojOcjena = 0;
 
-					if (!predmeti[i].empty())
-					{
-						cout << "Predmeti: ";
-						for (const string& predmet : predmeti[i])
-						{
-							cout << predmet << ", ";
+	srand(time(NULL));
+
+	while (1)
+	{
+		system("cls");
+		cout << "Dobrodosli u I-dnevnik" << endl;
+		cout << "1. Unos ucenika" << endl;
+		cout << "2. Ispis svih ucenika" << endl;
+		cout << "3. Brisanje ucenika" << endl;
+		cout << "4. Unos novih ocjena" << endl;
+		cout << "5. Ispis ocjena" << endl;
+		cout << "6. Slucajni odabir" << endl;
+		cout << "7. Izlaz iz programa" << endl;
+		cout << "Vas odabir je: ";
+		cin >> izbor;
+		if (izbor == 1)
+		{
+			cin.ignore();
+			cout << "Unesite ime i prezime ucenika: ";
+			getline(cin, ucenici[brojUcenika]);
+			brojUcenika++;
+		}
+		else if (izbor == 2)
+		{
+			if (brojUcenika > 0)
+			{
+				cout << "Popis ucenika" << endl;
+				cout << "---------------" << endl;
+
+				for (int i = 0; i < brojUcenika; i++)
+				{
+					cout << i << ". " << ucenici[i] << endl;
+				}
+			}
+			else
+			{
+				cout << "Nema unesenih ucenika." << endl;
+			}
+		}
+		else if (izbor == 3)
+		{
+			int makniUcenika;
+
+			if (brojUcenika > 0)
+			{
+				cout << "Popis ucenika" << endl;
+				cout << "---------------" << endl;
+
+				for (int i = 0; i < brojUcenika; i++)
+				{
+					cout << i << ". " << ucenici[i] << endl;
+				}
+
+				cout << "Unesite redni broj ucenika za brisanje: ";
+				cin >> makniUcenika;
+
+				for (int i = makniUcenika; i < brojUcenika; i++) {
+					ucenici[i] = ucenici[i + 1];
+				}
+
+				brojUcenika--;
+			}
+			else
+			{
+				cout << "Nema unesenih ucenika." << endl;
+			}
+		}
+		else if (izbor == 4)
+		{
+			int odabraniUcenik;
+			int odabraniPredmet;
+			int ocjena = 0;
+
+			if (brojUcenika > 0)
+			{
+				cout << "Popis ucenika" << endl;
+				cout << "---------------" << endl;
+
+				for (int i = 0; i < brojUcenika; i++)
+				{
+					cout << i << ". " << ucenici[i] << endl;
+				}
+
+				cout << "Unesite redni broj ucenika za unos ocjene: ";
+				cin >> odabraniUcenik;
+
+				cout << "Popis predmeta" << endl;
+				cout << "------------------" << endl;
+
+				for (int i = 0; i < sizeof(predmeti) / sizeof(string); i++)
+				{
+					cout << i << ". " << predmeti[i] << endl;
+				}
+
+				cout << "Unesite redni predmeta za unos ocjene: ";
+				cin >> odabraniPredmet;
+
+				cout << "Unesite ocjenu: ";
+				cin >> ocjena;
+
+				int pokazivac = brojOcjena * 3;
+
+				// upis ucenika
+				dnevnik[pokazivac] = odabraniUcenik;
+				// upis predmeta
+				dnevnik[pokazivac + 1] = odabraniPredmet;
+				// upis ocjene
+				dnevnik[pokazivac + 2] = ocjena;
+
+				brojOcjena++;
+			}
+			else
+			{
+				cout << "Nema unesenih ucenika." << endl;
+			}
+		}
+		else if (izbor == 5)
+		{
+			int odabraniUcenik;
+
+			if (brojUcenika > 0)
+			{
+				cout << "Popis ucenika" << endl;
+				cout << "---------------" << endl;
+
+				for (int i = 0; i < brojUcenika; i++)
+				{
+					cout << i << ". " << ucenici[i] << endl;
+				}
+
+				cout << "Unesite redni broj ucenika za ispis ocjena: ";
+				cin >> odabraniUcenik;
+
+				cout << "Ocjene za: " << ucenici[odabraniUcenik] << endl << endl;
+
+				// petlja po predmetima
+				int velicinaPredmeta = (sizeof(predmeti) / sizeof(string)) - 1;
+				for (int i = 0; i < velicinaPredmeta; i++) {
+					double suma = 0;
+					int brojacProsjeka = 0;
+
+					cout << predmeti[i] << " : ";
+
+					// petlja po dnevniku
+					// ocjene su zapisane u grupi po 3 -> broj ucenika, broj predmeta, ocjena
+					for (int j = 0; j < brojOcjena * 3; j = j + 3) {
+						if (dnevnik[j] == odabraniUcenik && dnevnik[j + 1] == i) {
+							int ocjena = dnevnik[j + 2];
+							cout << ocjena << ", ";
+							suma = suma + ocjena;
+							brojacProsjeka++;
 						}
-						cout << endl;
 					}
+
+					double prosjek = 0;
+					if (brojacProsjeka > 0) {
+						prosjek = suma / brojacProsjeka;
+					}
+
+					cout << "Prosjek: " << prosjek << endl;
 				}
 			}
 			else
 			{
 				cout << "Nema unesenih ucenika." << endl;
 			}
-        }
-        else if(izbor == 3)
-        {
-			cin.ignore();
-			cout << "Unesite redni broj ucenika: ";
-			unsigned long long int redniBroj;
-			cin >> redniBroj;
-			cin.ignore();
-
-			int index = find(redni, redni + br, redniBroj) - redni;
-			if (index == br)
-			{
-				cout << "Redni broj ucenika ne postoji." << endl;
-			}
-			else
-			{
-				cout << "Koliko predmeta zelite upisati?: ";
-				int brojPredmeta;
-				cin >> brojPredmeta;
-				cin.ignore();
-
-				cout << "Unesite predmete: " << endl;
-				for (int i = 0; i < brojPredmeta; i++)
-				{
-					string predmet;
-					getline(cin, predmet);
-					predmeti[index].push_back(predmet);
-				}
-			}
-        }
-        else if (izbor == 4)
-        {
-
-        }
-        else if (izbor == 5)
-        {
-			if (br > 0)
-			{
-				int slucajniUcenik = rand() % br;
-                cout << "Slucajni odabir je odabrao:" << endl;
-				cout << redni[slucajniUcenik]<<". ";
-				cout << prezimeIme[slucajniUcenik] << endl;
-			}
-			else
-			{
-				cout << "Nema unesenih ucenika." << endl;
-			}
-        }
-        else if (izbor == 6)
-        {
-			unsigned long long int unosUcenik;
-			{
-				cout << "Unesite redni broj ucenika: ";
-				cin >> unosUcenik;
-				int makni = find(redni, redni + br, unosUcenik) - redni;
-				if (makni == *redni + br)
-				{
-					cout << "Redni broj ucenika ne postoji." << endl;
-				}
-				else
-				{
-					for (int i = makni; i < br - 1; i++)
-					{
-						redni[i] = redni[i + 1];
-						prezimeIme[i] = prezimeIme[i + 1];
-					}
-					br--;
-				}
-			}
-        }
-        else if (izbor == 7)
-        {
-            cout << "Izlaz iz programa!";
-            break;
-        }
-        else
-        {
-            cout << "Krivi unos!" << endl;
-        }
-        pause();
-    }
-    return 0;
+		}
+		else if (izbor == 6)
+		{
+			int slucajniUcenik = rand() % brojUcenika;
+			cout << "Slucajni odabir je odabrao:" << endl;
+			cout << slucajniUcenik << ". " << ucenici[slucajniUcenik] << endl;
+		}
+		else if (izbor == 7)
+		{
+			cout << "Izlaz iz programa!";
+			break;
+		}
+		else
+		{
+			cout << "Krivi unos!" << endl;
+		}
+		pause();
+	}
+	return 0;
 }
